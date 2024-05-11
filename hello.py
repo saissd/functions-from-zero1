730a9a9dcf62ddcf01df2d39914b8a635dac776f
SAIDESHITH <110099288+saissd@users.noreply.github.com>
2024-05-11 07:36:19 +0000

    finalizing statements

--- hello.py
+++ hello.py
@@ -1,2 +1,9 @@
-x = 1
-y = 2
+# create a list with different types of fruits
+fruit = ["apples", "pears", "oranges", "peaches"]
+
+# loop through the fruits to print the fruit names and the no of fruots in the list
+for i in fruit:
+    print(f"{i} is the fruit number{fruit.index(i)+1}")
+
+# the total no of fruits in the list
+print(f"there are {len(fruit)}fruits in the list")
